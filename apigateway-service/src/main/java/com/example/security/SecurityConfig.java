03b430eddadfa0a1f19c49a8ffbfa9f49a28008e
jimesh <jnnaik88@gmail.com>
2024-11-28 10:28:00 +0530

    security key configuration

--- apigateway-service/src/main/java/com/example/security/SecurityConfig.java
+++ apigateway-service/src/main/java/com/example/security/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,23 +22,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.jwtUtils = jwtUtils;
     }
 
-    // Configure the AuthenticationManager to authenticate users
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
-    // Configure HttpSecurity to disable CSRF and enable JWT authentication
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()  // Disable CSRF protection
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/login").permitAll()  // Allow login requests without authentication
-                .antMatchers("/cart/**", "/products/**").authenticated()  // Protect /cart and /products paths
-                .anyRequest().permitAll()  // Allow other paths without authentication
+                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/cart/**", "/products/**").authenticated()
+                .anyRequest().permitAll()
                 .and()
-                .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtUtils)); // Add JWT filter
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(authenticationManagerBean(), jwtUtils), UsernamePasswordAuthenticationFilter.class);
     }
 }
